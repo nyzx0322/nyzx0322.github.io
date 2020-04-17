a18824d2bd00842387d1a636b5368d62a0ee2282
nyzx0322 <49115846+nyzx0322@users.noreply.github.com>
2020-04-17 23:04:07 +0800

    Delete index_banner.js

--- hswz/hswz/more/js/index_banner.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-document.writeln("<div class=\'banner\'>");
-document.writeln("<a href=\'http://nyzx0322.cn/\' target=\'_blank\' rel=\'external nofollow\'><img src=\'http://nyzx0322.cn/ziji/11研若科技11.png\' alt=\'\' title=\'\'></a>"); 	
-document.writeln("<a href=\'http://nyzx0322.cn/\' target=\'_blank\' rel=\'external nofollow\'><img src=\'http://nyzx0322.cn/ziji/11研若科技11.png\' alt=\'\' title=\'\'></a>"); 	
-document.writeln("</div>");
